b563ffefb9ac117b19262c46ddd2b5114d904d2e
Mohsin Sarker <mohsin.312007@gmail.com>
2025-07-24 16:59:46 +0100

    Refactor boto3 client

--- utils/generate_pdf.py
+++ utils/generate_pdf.py
@@ -10,9 +10,9 @@ S3_BUCKET_NAME = "participant-consent-forms"
 # Initialize an S3 Client
 s3_client = boto3.client(
         's3',
-        aws_access_key_id=st.secrets["aws"]["AWS_ACCESS_KEY_ID"],
-        aws_secret_access_key=st.secrets["aws"]["AWS_SECRET_ACCESS_KEY"],
-        region_name=st.secrets["aws"]["AWS_DEFAULT_REGION"]
+        aws_access_key_id=st.secrets["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=st.secrets["AWS_SECRET_ACCESS_KEY"],
+        region_name=st.secrets["AWS_DEFAULT_REGION"]
     )
 
 # Generate pdf
